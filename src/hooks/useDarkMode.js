5cbfae4b1ec05193bf3fbce40abdd9bd2d9581aa
Robert <rc3331@gmail.com>
2019-10-02 14:07:11 -0500

    committing mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,18 @@
+import useLocalStorage from "./useLocalStorage"
+import { useEffect } from "react"
+
+const useDarkMode = () => {
+   const [darkMode, setDarkMode] = useLocalStorage('dark')
+
+   useEffect(() => {
+       if (darkMode === true){
+        document.querySelector('body').classList.add('dark-mode')
+       } else {
+        document.querySelector('body').classList.remove('dark-mode')
+       }
+   }, [darkMode])
+
+   return [darkMode, setDarkMode]
+}
+
+export default useDarkMode
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+const useLocalStorage = (key, intialValue) => {
+    const [storedValue, setStoredValue] = useState(()=>{
+        const item = window.localStorage.getItem(key)
+        return item ? JSON.parse(item) : intialValue
+    })
+    
+
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    return [storedValue, setValue]
+}
+
+export default useLocalStorage
